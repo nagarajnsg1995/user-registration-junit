3200a3340922d9ff54cdbdec2b6bf898dd7eddcf
nagarajnsg1995 <nagarajsbg023@gmail.com>
2022-07-02 14:03:55 +0530

    Added: Email validaation to user registration

--- src/test/java/org/example/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/org/example/com/bridgelabz/UserRegistrationTest.java
@@ -21,7 +21,42 @@ public class UserRegistrationTest {
     }
 
     @Test
-    public void giveEmailVaule_WhenProper_ShouldReturnTrue() {
+    public void giveEmailVaule_WhenProper_ShouldReturnTrue1() {
+        boolean result = userRegistration.emailValidation("abc@yahoo.com");
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void giveEmailVaule_WhenProper_ShouldReturnTrue2() {
+        boolean result = userRegistration.emailValidation("nagaraja09@gmail.com");
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void giveEmailVaule_WhenProper_ShouldReturnTrue3() {
+        boolean result = userRegistration.emailValidation("nagarajaece.rymec@gmail.com");
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void giveEmailVaule_WhenProper_ShouldReturnTrue4() {
+        boolean result = userRegistration.emailValidation("nagarajsg023@gmail.com");
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void giveEmailVaule_WhenProper_ShouldReturnTrue5() {
+        boolean result = userRegistration.emailValidation("abc.100@yahoo.com");
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void giveEmailVaule_WhenProper_ShouldReturnTrue6() {
+        boolean result = userRegistration.emailValidation("abc+100@yahoo.com");
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void giveEmailVaule_WhenProper_ShouldReturnTrue7() {
+        boolean result = userRegistration.emailValidation("abc-100@yahoo.com");
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void giveEmailVaule_WhenProper_ShouldReturnTrueg8() {
         boolean result = userRegistration.emailValidation("abc@yahoo.com");
         Assert.assertTrue(result);
     }
